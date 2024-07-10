a393ec1bb88bbdb527e441c344a56c69a623851d
Sujal Pahwa <142010621+sujalpahwa30@users.noreply.github.com>
2024-07-10 16:09:29 +0530

    Feat : Added error handler middleware with basic error classes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 
 const {PORT} = require('./config/server.config');
 const apiRouter = require('./routes');
+const errorHandler = require('./utils/errorHandler');
 
 const app = express();
 
@@ -16,6 +17,9 @@ app.use('/api' , apiRouter);
 app.get('/ping' , (req , res) => {
     return res.json({message : 'Problem Service is alive'});
 });
+
+// last middleware if any error comes 
+app.use(errorHandler);
 
 app.listen(PORT , () => {
     console.log(`Server started at PORT : ${PORT}`);
