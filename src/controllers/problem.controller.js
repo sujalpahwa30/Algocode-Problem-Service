dc63516e42ca3c92f739da92339feca0e5988c51
Sujal Pahwa <142010621+sujalpahwa30@users.noreply.github.com>
2024-07-24 20:28:14 +0530

    Performed unit testing with jest

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -6,7 +6,7 @@ const {StatusCodes} = require('http-status-codes');
 const problemService = new ProblemService(new ProblemRepository());
 
 function pingProblemController(req , res) {
-    return res.json({message : 'Problem controller is up'});
+    return res.status(200).json({message : 'Problem controller is up'});
 }
 
 async function addProblem(req , res , next) {
